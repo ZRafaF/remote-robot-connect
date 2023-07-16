2fba03a9eb39e20d798a42dd00a5f76a6c387991
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-07-16 15:44:30 -0300

    added the android prebuild

--- App.tsx
+++ App.tsx
@@ -4,47 +4,58 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import { al } from "./src/helper/blAPI";
 
 import { BleManager } from "react-native-ble-plx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import useBLE from "./src/hooks/useBle";
+import DeviceModal from "./src/components/DeviceConnectionModal";
 
 export const manager = new BleManager();
 
 export default function App() {
-	function scanAndConnect() {
-		manager.startDeviceScan(null, null, (error, device) => {
-			if (error) {
-				// Handle error (scanning will be stopped automatically)
-				return;
-			}
+	const {
+		requestPermissions,
+		scanForPeripherals,
+		allDevices,
+		connectToDevice,
+		connectedDevice,
+		heartRate,
+		disconnectFromDevice,
+	} = useBLE();
 
-			// Check if it is a device you are looking for based on advertisement data
-			// or other criteria.
-			if (
-				device?.name === "TI BLE Sensor Tag" ||
-				device?.name === "SensorTag"
-			) {
-				// Stop scanning as it's not necessary if you are scanning for one device.
-				manager.stopDeviceScan();
+	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-				// Proceed with connection.
-			}
-		});
-	}
+	const scanForDevices = async () => {
+		const isPermissionsEnabled = await requestPermissions();
+		if (isPermissionsEnabled) {
+			scanForPeripherals();
+		}
+	};
 
-	useEffect(() => {
-		const subscription = manager.onStateChange((state) => {
-			if (state === "PoweredOn") {
-				scanAndConnect();
-				subscription.remove();
-			}
-		}, true);
-		return () => subscription.remove();
-	}, [manager]);
+	const hideModal = () => {
+		setIsModalVisible(false);
+	};
+
+	const openModal = async () => {
+		scanForDevices();
+		setIsModalVisible(true);
+	};
 
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
-			<Button title="mes" onPress={al} />
+			<Button title="mes" onPress={scanForDevices} />
+			<Button
+				title="Toggle Modal"
+				onPress={() => {
+					setIsModalVisible((old) => !old);
+				}}
+			/>
+			<DeviceModal
+				closeModal={hideModal}
+				visible={isModalVisible}
+				connectToPeripheral={connectToDevice}
+				devices={allDevices}
+			/>
 		</View>
 	);
 }
--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 
 # Remote Robot Connect
 
+## Project setup
+
+You can prebuild the app by running `npx expo prebuild`, this is needed for the BLE
+
 ## References
+
+When downloading depedencies use `npx expo install my-library`;
 
 -   Starting React native project https://www.youtube.com/watch?v=pflXnUNMsNk&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=2&ab_channel=TheNetNinja
