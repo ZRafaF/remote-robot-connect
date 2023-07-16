9c50b8fc88f8deba9c416b11fe24c936c5b98721
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-07-16 15:18:06 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import { al } from "./src/helper/blAPI";
+
+import { BleManager } from "react-native-ble-plx";
+import { useEffect } from "react";
+
+export const manager = new BleManager();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	function scanAndConnect() {
+		manager.startDeviceScan(null, null, (error, device) => {
+			if (error) {
+				// Handle error (scanning will be stopped automatically)
+				return;
+			}
+
+			// Check if it is a device you are looking for based on advertisement data
+			// or other criteria.
+			if (
+				device?.name === "TI BLE Sensor Tag" ||
+				device?.name === "SensorTag"
+			) {
+				// Stop scanning as it's not necessary if you are scanning for one device.
+				manager.stopDeviceScan();
+
+				// Proceed with connection.
+			}
+		});
+	}
+
+	useEffect(() => {
+		const subscription = manager.onStateChange((state) => {
+			if (state === "PoweredOn") {
+				scanAndConnect();
+				subscription.remove();
+			}
+		}, true);
+		return () => subscription.remove();
+	}, [manager]);
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+			<Button title="mes" onPress={al} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+<!--
+ Copyright (c) 2023 Rafael Farias
+
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+-->
+
+# Remote Robot Connect
+
+## References
+
+-   Starting React native project https://www.youtube.com/watch?v=pflXnUNMsNk&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=2&ab_channel=TheNetNinja
